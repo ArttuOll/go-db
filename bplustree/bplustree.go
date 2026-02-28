0be9c1a1535ba60d032cd9eac965ff84c3d217e4
Arttu Olli <60509537+ArttuOll@users.noreply.github.com>
2026-02-28 12:48:34 +0200

    4.3 Create B+tree nodes

--- bplustree/bplustree.go
+++ bplustree/bplustree.go
@@ -81,6 +81,20 @@ func (node Node) getOffset(index uint16) uint16 {
 	return binary.LittleEndian.Uint16(node[position:])
 }
 
+func (node Node) setOffset(index uint16, offset uint16) {
+	// The offset of the first key-value pair isn't stored
+	// because it's just 0
+	if index == 0 {
+		return
+	}
+
+	// Jump over the type, number of keys, the pointers and
+	// index - 1 offsets. Offsets are 2 bytes each.
+	position := 4 + 8*node.getNumberOfKeys() + 2*(index-1)
+
+	binary.LittleEndian.PutUint16(node[position:], offset)
+}
+
 func (node Node) keyValuePosition(index uint16) uint16 {
 	if index >= node.getNumberOfKeys() {
 		panic(1)
@@ -118,4 +132,28 @@ func (node Node) getValue(index uint16) []byte {
 	valueSize := binary.LittleEndian.Uint16(node[position+2:])
 
 	return node[position+4+keySize:][:valueSize]
+}
+
+func (node Node) appendKeyValue(index uint16, pointer uint64, key []byte, value []byte) {
+	node.setPointer(index, pointer)
+	position := node.keyValuePosition(index)
+
+	// Put the key size and value size in place
+	binary.LittleEndian.PutUint16(node[position:], uint16(len(key)))
+	binary.LittleEndian.PutUint16(node[position+2:], uint16(len(value)))
+
+	// Put the key and value in place
+	copy(node[position+4:], key)
+	copy(node[position+4+uint16(len(key)):], value)
+
+	// Update the offset for the next key.
+	// Offset for the key in the next index is the current offset plus
+	// the key and value sizes, then the sizes of the keys and values.
+	node.setOffset(index+1, node.getOffset(index)+4+uint16((len(key)+len(value))))
+}
+
+func (node Node) size() uint16 {
+	// The offset value of the last key indirectly tells us
+	// the size of the node
+	return node.keyValuePosition(node.getNumberOfKeys())
 }
